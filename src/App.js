af4b3219659e2754e5ef7252bc903a762faef4e1
LeuJiuChuan <jiuc0474@gmail.com>
2023-11-03 15:05:23 +0800

    UI: Navbar(halfdone) side bar done

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import {Footer,Sidebar,ThemeSettings, Navbar} from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const activeMenu = true;
+  const {activeMenu}=useStateContext();
+
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './contexts/ContextProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+
+    <ContextProvider>
+      <App />
+    </ContextProvider>,
+
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
